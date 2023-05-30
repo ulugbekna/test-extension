5a508308e69dff47e09bfbc9bc6214ea824c2db6
Ulugbek Abdullaev <ulugbekna@gmail.com>
2023-05-30 15:16:27 +0200

    try sahre provider API

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,31 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
+	
+	const docSelector : vscode.DocumentSelector =  { scheme: 'file', language: 'typescript' };
+	const shareProvider : vscode.ShareProvider = new class implements vscode.ShareProvider {
+		id = 'myId';
+		label = 'myLabel';
+		priority = 1;
+		async provideShare(item: vscode.ShareableItem, token: vscode.CancellationToken) {
+			const uri = item.resourceUri;
+			const selection = item.selection;
+			const document = await vscode.workspace.openTextDocument(uri);
+			const text = selection ? document.getText(selection) : document.getText();
+			const googleSearchLink = this.toGoogleSearchLink(text);
+			return googleSearchLink;
+		}
+		private toGoogleSearchLink(query: string): vscode.Uri {
+			const uri = vscode.Uri.parse('https://www.google.com/search');
+			const r = uri.with({
+				query: `q=${query}`
+			});
+			return r;
+		}
+	};
+	const shareProviderDisposable= vscode.window.registerShareProvider(docSelector, shareProvider);
+	context.subscriptions.push(shareProviderDisposable);
 }
+
 
 export function deactivate() {}
